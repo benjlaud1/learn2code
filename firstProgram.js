4b5f0858286f82535bcc4af8df9c832dcb49f663
Ben Lauderbaugh <benjlaud1@users.noreply.github.com>
2017-01-19 09:42:45 -0600

    Add files via upload

---
+++ firstProgram.js
@@ -0,0 +1,7 @@
+var firstTest = "WooHoo!!";
+var testNumber = 120;
+var testTrue = false;
+
+console.log("This is my first JavaScript program! " + firstTest);
+console.log("A number I chose at random: " + testNumber);
+console.log("Was this program a failure? " + testTrue);
