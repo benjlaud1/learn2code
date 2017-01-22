7ee763bfb658566fe85b04f65bb14e8e049e6b02
Ben Lauderbaugh <benjlaud1@users.noreply.github.com>
2017-01-21 22:03:20 -0600

    Update daily012117.js

--- daily012117.js
+++ daily012117.js
@@ -1,6 +1,7 @@
 var myAge = 30;
 var myWife = 'Cayla';
 var wifeAge = 33;
+var randNum = Math.random()
 
 // introduce self and wife
 
@@ -10,3 +11,8 @@ console.log('My wife\'s name is ' + myWife + '.');
 // compare ages
 
 console.log('She is ' + (wifeAge - myAge) + ' years older than me.');
+
+// math examples
+
+console.log('Random number: ' + randNum);
+console.log('Best year of my life: ' + Math.floor(randNum * myAge));
