6f909669b5859702025ce28a772673260fb06d2d
Ben Lauderbaugh <benjlaud1@users.noreply.github.com>
2017-01-21 21:51:57 -0600

    Daily 01-21-17

---
+++ daily012117.js
@@ -0,0 +1,12 @@
+var myAge = 30;
+var myWife = 'Cayla';
+var wifeAge = 33;
+
+// introduce self and wife
+
+console.log('Hello! I am a ' + myAge + ' year old male.');
+console.log('My wife\'s name is ' + myWife + '.');
+
+// compare ages
+
+console.log('She is ' + (wifeAge - myAge) + ' years older than me.');
